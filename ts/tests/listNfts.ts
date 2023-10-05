adf578d43d22a528bf2b3d7f9b1e87b7fc3bd6b1
Lythiuum <cryptolythium@gmail.com>
2023-10-05 18:26:05 +0100

    fixed listNfts test

--- ts/tests/listNfts.ts
+++ ts/tests/listNfts.ts
@@ -6,6 +6,9 @@ import { RafflesClient } from '@bankmenfi/raffles-client/client';
 import { CONFIGS } from '@bankmenfi/raffles-client/constants';
 import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet';
 import { ReadApiError } from '@metaplex-foundation/mpl-bubblegum';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { findMetadataPda, fetchMetadata } from '@metaplex-foundation/mpl-token-metadata';
+import { isSome, some } from '@metaplex-foundation/umi';
 
 // Load Env Variables
 require('dotenv').config({
@@ -16,6 +19,7 @@ require('dotenv').config({
 const CLUSTER = (process.env.CLUSTER as Cluster) || 'devnet';
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT || CONFIGS[CLUSTER].RPC_ENDPOINT;
 const KP_PATH = process.env.KEYPAIR_PATH;
+
 
 export const main = async () => {
   console.log(`Running listNfts. Cluster: ${CLUSTER}`);
@@ -31,30 +35,49 @@ export const main = async () => {
   );
 
   try {
-    const assets = await rafflesClient.umi.rpc.getAssetsByOwner({
-      owner: rafflesClient.umiPublicKey
-    });
-
-    if (assets.items.length == 0) {
-      console.log(`No NFTs found for owner ${rafflesClient.web3JsPublicKey}`);
-      return;
-    }
-    console.log(`Fetched ${assets.items.length} NFTs.`);
-
-    const filteredAssets = assets.items.filter(
-      (m) => !m.compression.compressed
+    const assets = await rafflesClient.connection.getParsedTokenAccountsByOwner(
+      wallet.publicKey,
+      { programId: TOKEN_PROGRAM_ID, }
     );
 
-    if (filteredAssets.length == 0) {
-      console.log(`No NFTs found for owner ${rafflesClient.web3JsPublicKey}`);
+    assets.value.forEach(e => console.log(e.account.data.parsed.info.tokenAmount.decimals)); 
+    console.log(assets.value.length);
+    const filteredValues = assets.value.filter((value) => {
+      return value.account.data.parsed.info.tokenAmount.decimals == 0;
+    });
+
+
+    if (filteredValues.length > 0) {
+      console.log("Filtered values:");
+      filteredValues.forEach(e => console.table(e.account.data.parsed.info));
+    } else {
+      console.log(`No NFTs found for owner ${rafflesClient.web3JsPublicKey}.`);
       return;
     }
 
-    console.table(filteredAssets);
+    const metadataList = filteredValues.map(
+      (value) => findMetadataPda(rafflesClient.umi, {
+        mint: value.account.data.parsed.info.mint
+      })
+    ).map(pda => fetchMetadata(rafflesClient.umi, pda));
+    
+    metadataList.forEach(e => e.then( e=> {
+      if (isSome(e.tokenStandard)){
+        console.log("Found NFT with name:");
+        console.log(e.name);
+        if (isSome(e.creators)){
+          console.log("The creators are:");
+          console.log(e.creators.value.forEach(e => console.log(e.address)));
+        }
+      }
+    } 
+    ));
+
   } catch (err) {
     if (err instanceof ReadApiError) {
       console.log(`ReadApiError: ${err.message}`);
     }
+    console.log(`err: ${err.message}`);
   }
 };
 
