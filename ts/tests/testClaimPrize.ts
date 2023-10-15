9adaa382fd5fea3d97c517531304216b24b81154
hoak <hoak@offpiste.io>
2023-10-15 15:29:17 +0100

    ts: changes to test claim prize

--- ts/tests/testClaimPrize.ts
+++ ts/tests/testClaimPrize.ts
@@ -7,7 +7,12 @@ import {
   PrizeAccount,
   RaffleAccount
 } from '@bankmenfi/raffles-client/accounts';
-import { PublicKey, Transaction } from '@solana/web3.js';
+import {
+  PublicKey,
+  SendTransactionError,
+  SolanaJSONRPCError,
+  Transaction
+} from '@solana/web3.js';
 import { CONFIGS } from '@bankmenfi/raffles-client/constants';
 import {
   fetchMetadata,
@@ -51,9 +56,7 @@ export const main = async () => {
   );
 
   for (let prizeIndex = 0; prizeIndex < raffle.prizes; prizeIndex++) {
-    const winnerTicketIndex =
-      expand(Buffer.from(raffle.state.randomness), prizeIndex) % entrants.total;
-    const winner = entrants.getEntrant(winnerTicketIndex);
+    // derive the prize address for the raffle and prize index
     const [prizeAddress] = derivePrizeAddress(
       RAFFLE,
       prizeIndex,
@@ -61,92 +64,148 @@ export const main = async () => {
     );
     console.log(`Prize: ${prizeAddress}`);
 
-    if (winner.equals(rafflesClient.web3JsPublicKey)) {
-      console.log(
-        `🎉🎉🎉 Congratulations!` +
-          `\nYour ticket #${winnerTicketIndex} won prize #${prizeIndex}.` +
-          `\nAttempting to claim prize..`
-      );
+    // load the prize account
+    const prize = await PrizeAccount.load(rafflesClient.program, prizeAddress);
 
-      const prize = await PrizeAccount.load(
-        rafflesClient.program,
-        prizeAddress
-      );
+    const tx = new Transaction();
+    let signature = null;
 
-      const tx = new Transaction();
+    let asset = null;
+    let metadataAccount = null;
+    let merkleTree = null;
+    let assetProof = null;
 
+    // check if the prize has an SPL token mint
+    // if so all we need to pass from the outside is the metadata account
+    if (!prize.state.mint.equals(PublicKey.default)) {
       const metadata = findMetadataPda(rafflesClient.umi, {
         mint: fromWeb3JsPublicKey(prize.state.mint)
       });
 
-      const metadataAccount = await fetchMetadata(rafflesClient.umi, metadata);
+      metadataAccount = await fetchMetadata(rafflesClient.umi, metadata);
+    } else {
+      // if not then we need to fetch the compressed NFT and merkle tree data etc
+      // TODO: do it
+      asset = null;
+      merkleTree = null;
+      assetProof = null;
+    }
 
-      const { ixs } = await prize.claimPrize(
-        raffle,
-        winnerTicketIndex,
-        null,
-        metadataAccount,
-        null,
-        null
-      );
+    // check if there are any entrants to the raffle
+    // if so then we can
+    if (entrants.total != 0) {
+      // because there are entrants to the raffle
+      // we need to expand the on-chain randomness for this specific prize index
+      // so we have the index of the winner's ticket and we can get the winner's pubkey!
+      const winnerTicketIndex =
+        expand(Buffer.from(raffle.state.randomness), prizeIndex) %
+        entrants.total;
+      const winner = entrants.getEntrant(winnerTicketIndex);
 
-      for (const ix of ixs) {
-        tx.add(ix);
+      // the client exposes two methods to claim the prizes
+      // one of them can be used by the client user
+      // the other is used to claim on behalf of prize winners.
+      if (winner.equals(rafflesClient.web3JsPublicKey)) {
+        console.log(
+          `🎉🎉🎉 Congratulations!` +
+            `\nYour ticket #${winnerTicketIndex} won prize #${prizeIndex}.` +
+            `\nAttempting to claim prize..`
+        );
+        const { ixs } = await prize.claimPrize(
+          raffle,
+          winnerTicketIndex,
+          asset,
+          metadataAccount,
+          merkleTree,
+          assetProof
+        );
+
+        for (const ix of ixs) {
+          tx.add(ix);
+        }
+      } else {
+        console.log(
+          `😭😭😭 Looks like you're shit outta luck today!` +
+            `\nPrize #${prizeIndex} was won by ticket #${winnerTicketIndex}, purchased by ${winner}.` +
+            `\nAttempting to claim on behalf of the winner..`
+        );
+        const { ixs } = await prize.claimForWinner(
+          raffle,
+          winner,
+          winnerTicketIndex,
+          asset,
+          metadataAccount,
+          merkleTree,
+          assetProof
+        );
+
+        for (const ix of ixs) {
+          tx.add(ix);
+        }
       }
 
-      const signature = await rafflesClient.program.sendAndConfirm(tx, [
-        wallet
-      ]);
-      console.log(`       Success!🎉`);
-      console.log(
-        `       ✅ - Claimed Prize #${prizeIndex} from Raffle ${raffle.address}.`
-      );
-      console.log(
-        `       ✅ Transaction - https://explorer.solana.com/tx/${signature}?cluster=${CLUSTER}`
-      );
+      try {
+        signature = await rafflesClient.program.sendAndConfirm(tx, [wallet]);
+        if (signature) {
+          console.log(`       Success!🎉`);
+          console.log(
+            `       ✅ - Claimed Prize #${prizeIndex} from Raffle ${raffle.address}.`
+          );
+          console.log(
+            `       ✅ Transaction - https://explorer.solana.com/tx/${signature}?cluster=${CLUSTER}`
+          );
+        }
+      } catch (e: unknown) {
+        if (e instanceof SolanaJSONRPCError) {
+          console.log(`JSON RPC Error: ${JSON.stringify(e)}`);
+        }
+        if (e instanceof SendTransactionError) {
+          console.log(`Send Transaction Error: ${JSON.stringify(e)}`);
+        }
+      }
     } else {
       console.log(
-        `😭😭😭 Looks like you're shit outta luck today!` +
-          `\nPrize #${prizeIndex} was won by ticket #${winnerTicketIndex}, purchased by ${winner}.` +
-          `\nAttempting to claim on behalf of the winner..`
+        `😭😭😭 Raffle with no entrants...` +
+          `\nAttempting to claim on behalf of the creator..`
       );
-      const prize = await PrizeAccount.load(
-        rafflesClient.program,
-        prizeAddress
-      );
-
-      const tx = new Transaction();
-
-      const metadata = findMetadataPda(rafflesClient.umi, {
-        mint: fromWeb3JsPublicKey(prize.state.mint)
-      });
-
-      const metadataAccount = await fetchMetadata(rafflesClient.umi, metadata);
 
       const { ixs } = await prize.claimForWinner(
         raffle,
-        winner,
-        winnerTicketIndex,
-        null,
+        raffle.creator,
+        0,
+        asset,
         metadataAccount,
-        null,
-        null
+        merkleTree,
+        assetProof
       );
+
+      for (const acc in ixs[0].keys) {
+        console.log(acc + ' ' + ixs[0].keys[acc].pubkey);
+      }
 
       for (const ix of ixs) {
         tx.add(ix);
       }
 
-      const signature = await rafflesClient.program.sendAndConfirm(tx, [
-        wallet
-      ]);
-      console.log(`       Success!🎉`);
-      console.log(
-        `       ✅ - Claimed Prize #${prizeIndex} from Raffle ${raffle.address}.`
-      );
-      console.log(
-        `       ✅ Transaction - https://explorer.solana.com/tx/${signature}?cluster=${CLUSTER}`
-      );
+      try {
+        signature = await rafflesClient.program.sendAndConfirm(tx, [wallet]);
+        if (signature) {
+          console.log(`       Success!🎉`);
+          console.log(
+            `       ✅ - Claimed Prize #${prizeIndex} from Raffle ${raffle.address}.`
+          );
+          console.log(
+            `       ✅ Transaction - https://explorer.solana.com/tx/${signature}?cluster=${CLUSTER}`
+          );
+        }
+      } catch (e: unknown) {
+        if (e instanceof SolanaJSONRPCError) {
+          console.log(`JSON RPC Error: ${JSON.stringify(e)}`);
+        }
+        if (e instanceof SendTransactionError) {
+          console.log(`Send Transaction Error: ${JSON.stringify(e)}`);
+        }
+      }
     }
   }
 };
