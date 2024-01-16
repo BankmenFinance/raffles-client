98b718bf1f91f03966dacda050bd83ac7716013f
hoak <hoak@offpiste.io>
2024-01-16 21:52:40 +0000

    adding method to retrieve winner by ticket index

--- rust/client/src/lib.rs
+++ rust/client/src/lib.rs
@@ -1,4 +1,7 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{
+    prelude::*,
+    solana_program::{keccak, pubkey::PUBKEY_BYTES},
+};
 
 anchor_gen::generate_cpi_interface!(idl_path = "idl.json");
 
@@ -91,5 +94,69 @@ impl std::fmt::Debug for WinnerRevealed {
             .field("raffle", &self.raffle)
             .field("randomness", &self.randomness)
             .finish()
+    }
+}
+
+impl Entrants {
+    /// The size of the account, not accounting for the Anchor Discriminator.
+    pub const LEN: usize = std::mem::size_of::<Self>();
+    /// The size of entrants excluding the entrants array.
+    pub const BASE_SIZE: usize = 8 + Self::LEN;
+
+    /// Gets the entrant for the given ticket index.
+    pub fn get_entrant_by_ticket_index(
+        entrants_data: &[u8],
+        max_entrants: usize,
+        ticket_index: usize,
+    ) -> Pubkey {
+        let mut i = 0;
+
+        for idx in 0..max_entrants {
+            let start_index = Entrants::BASE_SIZE + PUBKEY_BYTES * idx;
+            let entrant =
+                Pubkey::try_from_slice(&entrants_data[start_index..start_index + PUBKEY_BYTES])
+                    .unwrap();
+            if entrant != Pubkey::default() {
+                if i == ticket_index {
+                    return entrant;
+                } else {
+                    i += 1;
+                }
+            }
+        }
+
+        Pubkey::default()
+    }
+}
+
+pub fn expand(randomness: [u8; 32], n: u32) -> u32 {
+    let mut hasher = keccak::Hasher::default();
+    hasher.hash(&randomness);
+    hasher.hash(&n.to_le_bytes());
+
+    u32::from_le_bytes(
+        hasher.result().to_bytes()[0..4]
+            .try_into()
+            .expect("slice with incorrect length"),
+    )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_bit_slicing() {
+        let mut randomness = [0; 32];
+        randomness[0] = 1;
+        randomness[2] = 2;
+
+        let first_result = expand(randomness, 0);
+        println!("{}", first_result);
+
+        randomness[0] = 255;
+        randomness[1] = 254;
+
+        let second_result = expand(randomness, 1);
+        assert_ne!(first_result, second_result);
     }
 }
